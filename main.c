4e92109bdfbeb0084cdcb4249f9da0c799e2cd4c
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-08-30 18:09:26 -0300

    [ADD]: Gitignore and functions for initializing arrays

--- main.c
+++ main.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h> /* exit, malloc, calloc, etc. */
+#include <stdlib.h>
 
-#include "likwid.h"
+// #include "likwid.h"
 
 #define N 10000
 
-/**
- * Programa principal para testar configuraçãp do LIKWID
- */
+void initializeArrays(double ***A, double **b, double **x, int *n) {
+    scanf("%d", n);
+
+    *A = (double **)malloc((*n) * sizeof(double *));
+    for (int i = 0; i < *n; i++) {
+        (*A)[i] = (double *)malloc((*n) * sizeof(double));
+    }
+    *b = (double *)malloc((*n) * sizeof(double));
+    *x = (double *)malloc((*n) * sizeof(double));
+
+    for (int i = 0; i < *n; i++) {
+        for (int j = 0; j < *n; j++) {
+            scanf("%lf", &(*A)[i][j]);
+        }
+        scanf("%lf", &(*b)[i]);
+    }
+}
+
+void printInputs(double **A, double *b, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("| ");
+        for (int j = 0; j < n; j++) {
+            printf("%lf ", A[i][j]);
+        }
+        printf("| %lf\n", b[i]);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    LIKWID_MARKER_INIT;
+    // LIKWID_MARKER_INIT;
 
-    LIKWID_MARKER_START("Teste_LIKWID");
-    //
-    LIKWID_MARKER_STOP("Teste_LIKWID");
+    // LIKWID_MARKER_START("Teste_LIKWID");
+    // //
+    // LIKWID_MARKER_STOP("Teste_LIKWID");
 
-    LIKWID_MARKER_CLOSE;
+    // LIKWID_MARKER_CLOSE;
+
+    double **A = NULL, *b = NULL, *x = NULL;
+    int n;
+
+    initializeArrays(&A, &b, &x, &n);
+    printInputs(A, b, n);
 
     return 0;
 }
