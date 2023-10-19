cdef939fe2458f2dc040c77c912641eb6c985f96
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-10-19 19:59:06 -0300

    [ADD] Initial commit with data access optimizations
    Co-authored-by: YuriTobias <YuriTobias@users.noreply.github.com>

--- ci1164_2023-otimiz/matmult.c
+++ ci1164_2023-otimiz/matmult.c
@@ -1,23 +1,21 @@
+#include <getopt.h> /* getopt */
 #include <stdio.h>
-#include <stdlib.h>    /* exit, malloc, calloc, etc. */
+#include <stdlib.h> /* exit, malloc, calloc, etc. */
 #include <string.h>
-#include <getopt.h>    /* getopt */
 #include <time.h>
 
 #include "matriz.h"
+#include "utils.h"
 
 /**
  * Exibe mensagem de erro indicando forma de uso do programa e termina
  * o programa.
  */
 
-static void usage(char *progname)
-{
-  fprintf(stderr, "Forma de uso: %s [ <ordem> ] \n", progname);
-  exit(1);
+static void usage(char *progname) {
+    fprintf(stderr, "Forma de uso: %s [ <ordem> ] \n", progname);
+    exit(1);
 }
-
-
 
 /**
  * Programa principal
@@ -26,64 +24,83 @@ static void usage(char *progname)
  *
  */
 
-int main (int argc, char *argv[]) 
-{
-  int n=DEF_SIZE;
-  
-  MatRow mRow_1, mRow_2, resMat;
-  Vetor vet, res;
-  
-  /* =============== TRATAMENTO DE LINHA DE COMANDO =============== */
+int main(int argc, char *argv[]) {
+    int n = DEF_SIZE;
 
-  if (argc < 2)
-    usage(argv[0]);
+    MatRow mRow_1, mRow_2, resMat;
+    Vetor vet, res;
 
-  n = atoi(argv[1]);
-  
-  /* ================ FIM DO TRATAMENTO DE LINHA DE COMANDO ========= */
- 
-  srandom(20232);
-      
-  res = geraVetor (n, 0); // (real_t *) malloc (n*sizeof(real_t));
-  resMat = geraMatRow(n, n, 1);
-    
-  mRow_1 = geraMatRow (n, n, 0);
-  mRow_2 = geraMatRow (n, n, 0);
+    /* =============== TRATAMENTO DE LINHA DE COMANDO =============== */
 
-  vet = geraVetor (n, 0);
+    if (argc < 2) usage(argv[0]);
 
-  if (!res || !resMat || !mRow_1 || !mRow_2 || !vet) {
-    fprintf(stderr, "Falha em alocação de memória !!\n");
-    liberaVetor ((void*) mRow_1);
-    liberaVetor ((void*) mRow_2);
-    liberaVetor ((void*) resMat);
-    liberaVetor ((void*) vet);
-    liberaVetor ((void*) res);
-    exit(2);
-  }
-    
+    n = atoi(argv[1]);
+
+    /* ================ FIM DO TRATAMENTO DE LINHA DE COMANDO ========= */
+
+    srandom(20232);
+
+    res = geraVetor(n, 1);  // (real_t *) malloc (n*sizeof(real_t));
+    resMat = geraMatRow(n, n, 1);
+
+    mRow_1 = geraMatRow(n, n, 0);
+    mRow_2 = geraMatRow(n, n, 0);
+
+    vet = geraVetor(n, 0);
+
+    if (!res || !resMat || !mRow_1 || !mRow_2 || !vet) {
+        fprintf(stderr, "Falha em alocação de memória !!\n");
+        liberaVetor((void *)mRow_1);
+        liberaVetor((void *)mRow_2);
+        liberaVetor((void *)resMat);
+        liberaVetor((void *)vet);
+        liberaVetor((void *)res);
+        exit(2);
+    }
+
 #ifdef _DEBUG_
-    prnMat (mRow_1, n, n);
-    prnMat (mRow_2, n, n);
-    prnVetor (vet, n);
-    printf ("=================================\n\n");
+    prnMat(mRow_1, n, n);
+    prnMat(mRow_2, n, n);
+    prnVetor(vet, n);
+    printf("=================================\n\n");
 #endif /* _DEBUG_ */
 
-  multMatVet (mRow_1, vet, n, n, res);
-    
-  multMatMat (mRow_1, mRow_2, n, resMat);
-    
+    rtime_t multMatVetTime, multMatMatTime, multMatVetTimeOpt, multMatMatTimeOpt;
+    multMatVetTime = timestamp();
+    multMatVet(mRow_1, vet, n, n, res);
+    multMatVetTime = timestamp() - multMatVetTime;
+
+    multMatMatTime = timestamp();
+    multMatMat(mRow_1, mRow_2, n, resMat);
+    multMatMatTime = timestamp() - multMatMatTime;
+
+    printf("multMatVetTime: %lf\n", multMatVetTime);
+    printf("multMatMatTime: %lf\n", multMatMatTime);
+
+    res = geraVetor(n, 1);
+    resMat = geraMatRow(n, n, 1);
+
+    multMatVetTimeOpt = timestamp();
+    multMatVetOpt(mRow_1, vet, n, n, res);
+    multMatVetTimeOpt = timestamp() - multMatVetTimeOpt;
+
+    multMatMatTimeOpt = timestamp();
+    multMatMatOpt(mRow_1, mRow_2, n, resMat);
+    multMatMatTimeOpt = timestamp() - multMatMatTimeOpt;
+
+    printf("multMatVetTimeOpt: %lf\n", multMatVetTimeOpt);
+    printf("multMatMatTimeOpt: %lf\n\n", multMatMatTimeOpt);
+
 #ifdef _DEBUG_
-    prnVetor (res, n);
-    prnMat (resMat, n, n);
+    prnVetor(res, n);
+    prnMat(resMat, n, n);
 #endif /* _DEBUG_ */
 
-  liberaVetor ((void*) mRow_1);
-  liberaVetor ((void*) mRow_2);
-  liberaVetor ((void*) resMat);
-  liberaVetor ((void*) vet);
-  liberaVetor ((void*) res);
+    liberaVetor((void *)mRow_1);
+    liberaVetor((void *)mRow_2);
+    liberaVetor((void *)resMat);
+    liberaVetor((void *)vet);
+    liberaVetor((void *)res);
 
-  return 0;
+    return 0;
 }
-
